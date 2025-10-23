2a10f2b06947d2fed9f645d82a3c76629680f85e
Joao KeceK <jfabrao05@gmail.com>
2025-10-23 08:40:02 -0300

    ShopSO Figures

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -1,9 +1,35 @@
 
 public enum FigureType
 {
-    Jason,
-    IronMan,
-    IT,
+    C3POeR2D2,
+    ClaireRedfield,
+    DarthVader,
+    DeathTrooper,
+    EthanHunt,
+    ForrestGump,
+    FreddyKrueger,
+    IndianaJonesDestiny,
+    IndianaJonesLostArc,
+    JasonVoorhees,
+    Joker,
+    JonSnow,
+    Mark1,
+    Mark3,
+    Mark43,
+    MichaelMyers,
+    Neo,
+    Pennywise,
+    Pennywise1990,
+    PiratesOfCaribbeanDX06,
+    PiratesOfCaribbeanDX39AE,
+    Rambo,
+    Robocop,
+    RoyalDefender,
+    SpriderMan,
+    TonyStark,
+    WalterWhite,
+    WillianWallace,
+    WonderWoman,
 }
 
 public enum DaddyType
