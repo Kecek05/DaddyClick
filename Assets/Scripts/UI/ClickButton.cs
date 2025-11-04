96d577d6365260faf07c617e8f1f2d3b10c0c2be
Joao KeceK <jfabrao05@gmail.com>
2025-11-03 23:56:21 -0300

    Adjust to Manual Click Detection

--- Assets/Scripts/UI/ClickButton.cs
+++ Assets/Scripts/UI/ClickButton.cs
@@ -7,7 +7,10 @@ public class ClickButton : MonoBehaviour, IPointerClickHandler
 {
     public void OnPointerClick(PointerEventData eventData)
     {
-        ClickManager.ManualClick(1 + ClickManager.CPS, eventData.position);
+        if (eventData.pointerCurrentRaycast.gameObject == gameObject)
+        {
+            ClickManager.ManualClick(1 + ClickManager.CPS, eventData.position);
+        }
     }
     
 }
